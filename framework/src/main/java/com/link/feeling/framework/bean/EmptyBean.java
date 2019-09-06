5b3eb0bb7cf285e7fa05de9f83c11b12651da35e
chenpan <13667237026@163.com>
2019-09-06 15:36:20 +0800

    【需求】项目基础框架搭建

--- framework/src/main/java/com/link/feeling/framework/bean/EmptyBean.java
+++
@@ -1,9 +0,0 @@
-package com.link.feeling.framework.bean;
-
-/**
- * Created on 2019/1/14  17:13
- * chenpan pan.chen@linkfeeling.cn
- */
-@SuppressWarnings("unused")
-public final class EmptyBean {
-}
--- mvp/src/main/java/com/link/feeling/mvp/common/MvpView.kt
+++
@@ -1,8 +0,0 @@
-package com.link.feeling.mvp.common
-
-/**
- * Created on 2019/1/11  15:18
- * chenpan pan.chen@linkfeeling.cn
- */
-interface MvpView : UI {
-}
